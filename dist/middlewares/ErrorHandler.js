b403ac4006d7c23a2213126b1475b490deda27ec
Mohamed Fathy <mohammad.soliman@gmail.com>
2022-07-26 11:26:08 +0200

    changing files names

--- dist/middlewares/ErrorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ErrorHandler = (error, _req, res, next) => {
-    const status = error.status || 500;
-    const message = error.message || 'Error';
-    res.status(status).json({ status, message });
-};
-exports.default = ErrorHandler;
--- dist/models/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// structure it to be able to do:
-// import { UserService, ProductService } from '../services'
-// export { CourseStudentTable } from "./CourseStudent";
-// export { CourseTable } from "./Course";
